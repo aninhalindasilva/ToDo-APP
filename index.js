5a5e53634e2fcc18f4c4c9da7b7cb012268ef24e
Ana Clara <anne.silvajmd@gmail.com>
2023-11-15 15:13:43 -0300

    primeira execução do servidor

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const app = express()
+
+app.get('/', (requisicao, resposta) => {
+    resposta.send("Olá, mundo")
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 300!")
+})
+
+
