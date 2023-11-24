b60773804b42bccee8b58eb737479f83d650fd47
Ana Clara <anne.silvajmd@gmail.com>
2023-11-24 18:11:17 -0300

    iniciando a conexão com mysql

--- public/js/script.js
+++ public/js/script.js
@@ -1,46 +1,46 @@
 function alterarTema() {
-    const tema = localStorage.getItem("tema")
-    const body = document.querySelector("body")
-    const button = document.querySelector(".tema-button")
+    const tema = localStorage.getItem("tema");
+    const body = document.querySelector("body");
+    const button = document.querySelector(".tema-button");
 
     if (tema) {
-        let novoTema
+        let novoTema;
 
-        if(tema === "light"){
-            novoTema = "dark"
-            button.innerHTML = `<img src="../img/solcerto.png" alt="Ícone do sol">`
-            body.classList.remove("light")
-            body.classList.add("dark")
-        }  else {
-            button.innerHTML `<img src="../img/lua1.png" alt="Ícone de lua">`
-            novoTema = "light"
-            body.classList.remove("dark")
-            body.classList.add("light")
+        if (tema === "light") {
+            novoTema = "dark";
+            button.innerHTML = `<img src="/img/sol certo.png" alt="Ícone do sol">`;
+            body.classList.remove("light");
+            body.classList.add("dark");
+        } else {
+            novoTema = "light";
+            button.innerHTML = `<img src="../img/lua1.png" alt="Ícone de lua">`;
+            body.classList.remove("dark");
+            body.classList.add("light");
         }
 
-        localStorage.setItem("tema", novoTema)
-        return
+        localStorage.setItem("tema", novoTema);
+        return;
     }
 
-    localStorage.setItem("tema", "dark")
-    body.classList.add("dark")
+    localStorage.setItem("tema", "dark");
+    body.classList.add("dark");
 }
 
-function verificarTema(){
-    const tema = localStorage.getItem("tema")
-    const body = document.querySelector("body")
-    const button = document.querySelector(".tema-button")
-    
-    if (tema){
-        if (tema == "dark"){
-            body.classList.add("dark")
-            button.innerHTML = `<img src="../img/solcerto.png" alt="Ícone do sol">`
-        }  else {
-            body.classList.add("light")
-            button.innerHTML `<img src="../img/lua1.png" alt="Ícone de lua">`
+function verificarTema() {
+    const tema = localStorage.getItem("tema");
+    const body = document.querySelector("body");
+    const button = document.querySelector(".tema-button");
+
+    if (tema) {
+        if (tema === "dark") {
+            body.classList.add("dark");
+            button.innerHTML = `<img src="/img/sol certo.png" alt="Ícone do sol">`;
+        } else {
+            body.classList.add("light");
+            button.innerHTML = `<img src="../img/lua1.png" alt="Ícone de lua">`;
         }
     }
 }
 
-verificarTema()
+verificarTema();
 
